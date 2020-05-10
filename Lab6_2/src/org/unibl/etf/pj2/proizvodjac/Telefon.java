294011d1919820c241aac1db394b4cf645174c20
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2020-05-10 12:15:05 +0200

    Add files via upload

---
+++ Lab6_2/src/org/unibl/etf/pj2/proizvodjac/Telefon.java
@@ -0,0 +1,45 @@
+package org.unibl.etf.pj2.proizvodjac;
+
+import org.unibl.etf.pj2.proizvodi.Proizvod;
+
+public class Telefon extends Proizvod {
+    protected String conf;
+    protected String model;
+
+    public Telefon(){
+        super();
+    }
+
+    public Telefon(String password, double cost, String name, Proizvodjac user, String conf, String model){
+        super(password, cost, name, user);
+        this.conf = conf;
+        this.model = model;
+    }
+
+    public Telefon(String password, double cost, String name, String name1, String country, String email, String conf, String model){
+        super(password, cost, name, name1, country, email);
+        this.conf = conf;
+        this.model = model;
+    }
+
+    public String getConf() {
+        return conf;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setConf(String conf) {
+        this.conf = conf;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString(){
+        return FormatOutPut() + "Konfiguracija: " + conf + "\nModel: " + model + "\n";
+    }
+}
