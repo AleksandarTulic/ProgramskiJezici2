41200ecc2b202a8446336486553e057c52f5f521
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2020-05-10 14:03:40 +0200

    Add files via upload

---
+++ Lab7_02/src/JavaTechnoShop.java
@@ -0,0 +1,111 @@
+import org.unibl.etf.pj2.org.unibl.etf.pj2.cjenovnik;
+import org.unibl.etf.pj2.org.unibl.etf.pj2.racun;
+import org.unibl.etf.pj2.proizvodi.Proizvod;
+import org.unibl.etf.pj2.proizvodjac.*;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class JavaTechnoShop{
+    public static void main(String []args) throws IOException, ClassNotFoundException {
+        System.out.println("Shop :)");
+
+        Proizvodjac samsung = new Proizvodjac("Samsung", "South Korea", "samsung@gmail.com");
+        Proizvodjac lg = new Proizvodjac("LG", "South Korea", "Lginfo@gmail.com");
+        Proizvodjac nokia = new Proizvodjac("Nokia", "Finland", "groundBroken@hotmail.com");
+        Proizvodjac iphone = new Proizvodjac("Iphone", "United States of America", "IphoneInfo@gmail.us");
+
+        Monitor m1 = new Monitor("001", 250, "Lg smart screen", lg, "Resolution: 21:9", "001 Screen");
+        Monitor m2 = new Monitor("010", 120, "Lg screen", lg, "Resolution: 16:9", "010 Screen");
+        Monitor m3 = new Monitor("011", 419.9, "Lg smart screen X2", lg, "Resolution: 30:9", "011 Screen");
+        Softver a1 = new Softver("100", 31.9, "OS",iphone, "Very bad");
+        Telefon b1 = new Telefon("101", 788.1, "Iphone X", iphone, "weight: 100mg", "Model X");
+        Telefon b2 = new Telefon("110", 450, "Nokia 7799", nokia, "weight: 220mg, camera MP: 12px", "7799");
+        Racunar c1 = new Racunar("111", 1999.9, "Wargaming Extreme", samsung, "CPU: i7, 2.7GHz");
+        Racunar c2 = new Racunar("1000", 1431.4, "Office machine", iphone, "CPU: double core, 2.1GHz");
+
+        cjenovnik a = new cjenovnik();
+        a.a = new Proizvod[]{m1, m2, a1, m3, b1, b2, c1, c2};
+
+        a.printProizvodi();
+        if ( a.getProizvodSaSifrom("010") == null ){
+            System.out.println("Proizvod sa sifrom 010 ne postoji.");
+        }else{
+            System.out.println("Proizvod sa sifrom 010 postoji.");
+        }
+
+        racun arr = new racun();
+        Scanner scan = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Pregled cjenovnika                 [1]");
+            System.out.println("Pregled proizvoda po sifri         [2]");
+            System.out.println("Pregled Proizvoda po nazivu        [3]");
+            System.out.println("Kupovina proizvoda                 [4]");
+            System.out.println("Prikaz kupovie                     [5]");
+            System.out.println("Ukloniti proizod sa liste kupovine [6]");
+            System.out.println("Prikazati sve racune               [7]");
+            System.out.println("Zakljuci kupovinu                  [8]");
+
+            System.out.print("Unesite opciju: ");
+            int option = scan.nextInt();
+
+            if ( option == 1 ){
+                a.printProizvodi();
+            }else if ( option == 2 ){
+                System.out.println("Unesite sifru: ");
+                Scanner s = new Scanner(System.in);
+                String b = s.nextLine();
+                System.out.println(b);
+                a.printProizvodiPassword(b);
+            }else if ( option == 3 ){
+                System.out.println("Unesite naziv: ");
+                Scanner s = new Scanner(System.in);
+                String b = s.nextLine();
+                a.printProizvodiName(b);
+            }else if ( option == 4 ){
+                System.out.println("Unesite sifru proizvoda sto kupujete: ");
+                Scanner s = new Scanner(System.in);
+                String b = s.nextLine();
+                arr.addProizvod(a.getProizvodSaSifrom(b));
+            }else if ( option == 5 ){
+                arr.showList();
+            }else if ( option == 6 ){
+                System.out.println("Unesite sifru proizvoda koji zelite da obrisete: ");
+                Scanner s = new Scanner(System.in);
+                String b = s.nextLine();
+                arr.deleteProizvod(a.getProizvodSaSifrom(b));
+            }else if ( option == 7 ){
+                System.out.println("Svi racuni: ");
+
+                File nameFile = new File("D:\\FAKULTET");
+                File []listFile = nameFile.listFiles();
+
+                for (File element : listFile){
+                    if ( element.isFile() ){
+                        System.out.println(element);
+
+                        ObjectInputStream obj = new ObjectInputStream(new FileInputStream(element));
+                        racun r = (racun)obj.readObject();
+                        r.buy();
+                    }
+                }
+            }
+            else if (option == 8){
+                arr.buy();
+                String dod = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy-HH-mm-ss"));
+                dod+=".etf";
+                ObjectOutputStream obj = new ObjectOutputStream(new FileOutputStream("D:\\FAKULTET\\" + dod));
+                obj.writeObject(arr);
+                obj.close();
+                break;
+            }else{
+                System.out.println("niste nesto dobro unijeli!!!\nPokusajte ponovo!!!");
+            }
+        }
+    }
+}
---
+++ Lab7_02/src/org/unibl/etf/pj2/proizvodjac/Racunar.java
@@ -0,0 +1,36 @@
+package org.unibl.etf.pj2.proizvodjac;
+
+import org.unibl.etf.pj2.proizvodi.Proizvod;
+
+import java.io.Serializable;
+
+public class Racunar extends Proizvod implements Serializable {
+    protected String conf;
+
+    public Racunar(){
+        super();
+    }
+
+    public Racunar(String password, double cost, String name, Proizvodjac user, String conf){
+        super(password, cost, name, user);
+        this.conf = conf;
+    }
+
+    public Racunar(String password, double cost, String name, String name1, String country, String email, String conf){
+        super(password, cost, name, name1, country, email);
+        this.conf = conf;
+    }
+
+    public String getConf() {
+        return conf;
+    }
+
+    public void setConf(String conf) {
+        this.conf = conf;
+    }
+
+    @Override
+    public String toString(){
+        return FormatOutPut() + "Konfiguracija: " + conf + "\n";
+    }
+}
