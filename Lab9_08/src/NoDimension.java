aceb712db5f0c1794412bd892100f653fb74c9ab
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2020-05-20 17:05:38 +0200

    Add files via upload

---
+++ Lab9_08/src/NoDimension.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class NoDimension extends Exception {
+    public NoDimension(){
+        super();
+    }
+
+    public NoDimension(String message){
+        super(message);
+    }
+}
