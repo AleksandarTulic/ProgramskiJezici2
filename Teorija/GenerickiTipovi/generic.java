7a346b9e0ef55de426b9fd6dbc10382a9f710334
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2020-05-21 12:41:40 +0200

    Create generic.java

---
+++ Teorija/GenerickiTipovi/generic.java
@@ -0,0 +1,72 @@
+/*
+
+    - GENERICI:
+        - IZBJEGAVAMO KASTOVANJE
+            ArrayList<String> a = new ArrayList<String>();
+            a.add("Nesto1");
+            a.add("Nesto2");
+            String s = a.get(0); - NIJE POTREBAN CAST
+
+        - MOZEMO DETEKTOVATI RAZNE GRESKE
+            - ZNA SE U VRIJEME KOMPJALIRANJA DA JE GRESKA(DOK OVAKO SE INACE DESAVA ZA VRIJEME RUN TIME)
+
+        - NPR. AKO IMAMO ARRAYLIST KOJA JE TIP STRING-A KADA SE UNESE U NJEGA TIP INT ON CE NORMALNO RADITI ALI KADA BUDE
+    TREBALO DOHVATITI ONDA CE DOCI DO GRESKE
+            ArrayList a = new ArrayList();
+            a.add("Nesto1");
+            a.add("Nesto2");
+            a.add(10);
+            String s = (String)a.get(2) - GRESKA!!! RUN TIME - KOMPAJLER ODOBRAVA
+
+        - PARAMETRIZOVANI TIP:
+            - KORISTENJE GENERICKOG TIPA GDJE SU TIPSKE PROMJENLJIVE ZAMIJENJENE PARAMETRIMA STVARNOG TIPA
+            - POTREBNO JE IZBJEGAVATI KORISTENJE RAW TIPA - Integer, String, Boolean ...
+
+        - GENERICKI TIP KOJI NIJE DEKLARISAN KAO FINAL MOZE BITI PROSLIJEDJEN
+            - FINAL ZNACI DA SE VRIJEDNOST NE MOZE MIJENJATI - UKOLIKO SE POKUSA GRESKA
+            - UKOLIKO JE NEKA METODA FINAL ONA SE NE MOZE OVERRIDE
+            - UKOLIKO JE NEKA KLASA FINAL ONDA SE ONA NE MOZE EXTEND-OVATI
+
+        - NASLJEDJIVANJE:
+            - MOGUCE JE DA GENERICKI TIP NASLIJEDI NEGENERICKI
+
+                public class Something1<T> extends Something2{...}
+
+            - MOGUCE JE DA KONKRETAN TIP NASLIEDI GENERICKI
+
+                public class Something2 extends Something1<t>{...}
+         - OGRANICAVANJE
+
+            public class Something1<T extends Something2> extends Something3<T>{...}
+                - ZNACI DA KAO PARAMETAR MOZE BITI SAMO SOMETHING2 ILI NJEGOVI NASLJEDNICI I KLASE SOMETHING3
+
+
+         - METODE
+
+            static <T> void somethingHappened(){...}
+
+         - WILDCARDS - ?
+
+            - MOZE SE KORISTIT:
+                - KAO TIP PARAMETRA, POLJA, LOKALNE VARIJABLE
+
+            - UPPER BOUNDED:
+                - SMANJIMO RESTRIKCIJE NA VARIJABLAMA
+
+                public static void add(List <? extends Number> list){...}
+                    - gornja granica je NUMBER, i obuhvata tipove kao sto su double, int
+
+            - LOWER BOUND:
+
+                public static void something(List <? super Integer> list){...}
+                    - uzima samo int i njegovu nad klasu koja je number - nece double
+
+            - UNBOUNDED:
+                - KADA SADRZAJ STO RADIMO NE ZAVISI OD TIPA KAO NPR ISPIS NIZA NEKIH ELEMENATA
+
+                public static void something(List <?> list){ sout(list) }
+                
+
+*/
+public class generic {
+}
